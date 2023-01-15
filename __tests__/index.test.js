429f32a224eb74d42a47b64c20e7294b46818570
Юрий Шершнев <yuramummy@mail.ru>
2023-01-15 22:39:33 +0300

    changed test file name and added resultJSON file

---
+++ __tests__/index.test.js
@@ -0,0 +1,6 @@
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+// import fs from 'fs';
+// import genDiff from '../index.js';
+
+// const resultJSON = fs.readFileSync('resultJSON.txt');
