20ebf24b457fdc3f1a6e7c2ff592a646b167af6e
Юрий Шершнев <yuramummy@mail.ru>
2023-01-30 19:37:33 +0300

    fixed linter errors

--- src/parser.js
+++ src/parser.js
@@ -1,12 +1,12 @@
 import yaml from 'js-yaml';
 
 export default (data, format) => {
-    switch (format) {
-        case 'yml':
-        case 'yaml':
-            return yaml.load(data);
-        case 'json':
-        default:
-            return JSON.parse(data);
-    }
+  switch (format) {
+    case 'yml':
+    case 'yaml':
+      return yaml.load(data);
+    case 'json':
+    default:
+      return JSON.parse(data);
+  }
 };
