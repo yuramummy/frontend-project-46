a165476ff53e8da54099f4411f31f3518797f053
Юрий Шершнев <yuramummy@mail.ru>
2023-01-30 19:24:48 +0300

    add parser.js

---
+++ src/parser.js
@@ -0,0 +1,12 @@
+import yaml from 'js-yaml';
+
+export default (data, format) => {
+    switch (format) {
+        case 'yml':
+        case 'yaml':
+            return yaml.load(data);
+        case 'json':
+        default:
+            return JSON.parse(data);
+    }
+};
