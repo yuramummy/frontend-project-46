0b1a94362999fe1acc8d672247d1b82796ee6b07
Юрий Шершнев <yuramummy@mail.ru>
2023-02-04 23:42:10 +0300

    added stylish.js

---
+++ src/stylish.js
@@ -0,0 +1,45 @@
+import _ from 'lodash';
+
+const replacer = ' ';
+const spacesCount = 4;
+
+const getIndent = (depth) => replacer.repeat(depth * spacesCount - 2);
+const getBracketIndent = (depth) => replacer.repeat(depth * spacesCount - spacesCount);
+
+const stringify = (data, depth) => {
+  if (!_.isObject(data)) {
+    return `${data}`;
+  }
+
+  const entries = Object.entries(data);
+  const lines = entries.map(([key, val]) => `${getIndent(depth)}  ${key}: ${stringify(val, depth + 1)}`);
+  return ['{', ...lines, `${getBracketIndent(depth)}}`].join('\n');
+};
+
+const getString = (array) => {
+  const iter = (node, depth) => {
+    const lines = node.map((data) => {
+      const {
+        type, key, value, addedValue, deletedValue, children,
+      } = data;
+
+      if (type === 'parent') {
+        return `${getIndent(depth)}  ${key}: ${iter(children, depth + 1)}`;
+      }
+      if (type === 'added') {
+        return `${getIndent(depth)}+ ${key}: ${stringify(value, depth + 1)}`;
+      }
+      if (type === 'deleted') {
+        return `${getIndent(depth)}- ${key}: ${stringify(value, depth + 1)}`;
+      }
+      if (type === 'unchanged') {
+        return `${getIndent(depth)}  ${key}: ${stringify(value, depth + 1)}`;
+      }
+      return `${getIndent(depth)}- ${key}: ${stringify(deletedValue, depth + 1)}\n${getIndent(depth)}+ ${key}: ${stringify(addedValue, depth + 1)}`;
+    });
+    return ['{', ...lines, `${getBracketIndent(depth)}}`].join('\n');
+  };
+  return iter(array, 1);
+};
+
+export default getString;
