8ed0fac8b9017e37122b12fdb725edb6ef91e573
Юрий Шершнев <yuramummy@mail.ru>
2023-02-13 15:28:48 +0300

    added getFormat function

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import { readFileSync } from 'node:fs';
 import parse from './src/parser.js';
-import getDiff from './src/get-diff.js';
-import getString from './src/stylish.js';
+import getFormat from './src/formatters/index.js';
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, format) => {
   const data1 = readFileSync(filepath1, 'utf-8'); // читаем первый файл
   const data2 = readFileSync(filepath2, 'utf-8'); // читаем второй файл
 
@@ -12,7 +11,7 @@ const genDiff = (filepath1, filepath2) => {
   const obj1 = parse(data1, filepath1.split('.')[1]);
   const obj2 = parse(data2, filepath2.split('.')[1]);
 
-  return getString(getDiff(obj1, obj2));
+  return getFormat(obj1, obj2, format);
 };
 
 export default genDiff;
