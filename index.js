8ca8ed0e03f625ea476dcf18b734b2a0dae7039d
Юрий Шершнев <yuramummy@mail.ru>
2023-02-24 10:24:04 +0300

    index.js changed to re-export genDiff function

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-import { readFileSync } from 'node:fs';
-import parse from './src/parser.js';
-import getFormat from './src/formatters/index.js';
-
-const genDiff = (filepath1, filepath2, format) => {
-  const data1 = readFileSync(filepath1, 'utf-8'); // читаем первый файл
-  const data2 = readFileSync(filepath2, 'utf-8'); // читаем второй файл
-
-  // парсим данные из файла на основе его расширения с помощью функции parse
-  // результаты полученных объектов присваиваем переменным
-  const obj1 = parse(data1, filepath1.split('.')[1]);
-  const obj2 = parse(data2, filepath2.split('.')[1]);
-
-  return getFormat(obj1, obj2, format);
-};
+import genDiff from './src/index.js';
 
 export default genDiff;
